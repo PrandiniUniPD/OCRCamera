1b62a9b6e32f36d1ba11ee07584d03bb5ed7d135
Giofasan <giovanni.fasan@studenti.unipd.it>
2018-11-02 17:37:45 +0100

    Second Commit
    Aggiunta ml-kit.
    problema: immagine storta

--- app/src/main/java/unipd/se18/ocrcamera/ResultActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/ResultActivity.java
@@ -1,16 +1,39 @@
 package unipd.se18.ocrcamera;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Class used for showing the result of the OCR processing
  */
 public class ResultActivity extends AppCompatActivity {
 
+    Bundle extras;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        extras = getIntent().getExtras();
+        String path=extras.getString("PATH_I_NEED");
+
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        TextView textView = (TextView) findViewById(R.id.ocr_text_view);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        TextExtractor manager = new TextExtractor();
+        String text = manager.getTextFromImg(bitmap);
+
+        ImageView imageView = (ImageView) findViewById(R.id.img_captured_view);
+        imageView.setImageBitmap(bitmap);
+        textView.setText(text);
+
     }
 }
--- app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
+++ app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
@@ -1,11 +1,21 @@
 package unipd.se18.ocrcamera;
 
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 /**
  * Implements the common OCR wrapper to retrieve text from an image.
  */
-class TextExtractor implements OCRWrapper {
+class TextExtractor implements OCRInterface {
+
+    private String txtresult="Testo: \n";
 
     /**
      * Extract a text from a given image.
@@ -14,6 +24,25 @@ class TextExtractor implements OCRWrapper {
      */
     @Override
     public String getTextFromImg(Bitmap img) {
-        return "";
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(img);
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText result) {
+                txtresult=result.getText();
+            }
+        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                                txtresult="Fail";
+                            }
+                        });
+        return txtresult;
     }
+
 }
