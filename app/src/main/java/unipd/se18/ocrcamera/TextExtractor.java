c885aff65b23653f17f9486cfc32520635c6dabc
Giofasan <giovanni.fasan@studenti.unipd.it>
2018-11-03 12:09:17 +0100

    aggiornamento senza successo

--- app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
+++ app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
@@ -1,11 +1,25 @@
 package unipd.se18.ocrcamera;
 
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.List;
 
 /**
  * Implements the common OCR wrapper to retrieve text from an image.
  */
 class TextExtractor implements OCRInterface {
+
+    private String txtresult="Testo: \n";
 
     /**
      * Extract a text from a given image.
@@ -14,6 +28,28 @@ class TextExtractor implements OCRInterface {
      */
     @Override
     public String getTextFromImg(Bitmap img) {
-        return "";
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(img);
+        final FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText result) {
+                txtresult = txtresult + result.getText();
+                Log.d("CameraActivity", txtresult);
+                Log.d("CameraActivity", "Testo riconosciuto");
+            }
+        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                                txtresult="Fail";
+                                Log.d("CameraActivity", "Testo NON riconosciuto");
+
+                            }
+                        });
+        return txtresult;
     }
+
 }
