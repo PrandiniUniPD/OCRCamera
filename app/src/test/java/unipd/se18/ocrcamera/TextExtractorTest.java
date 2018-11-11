528a482ad520a837ea06335755f51930995b6cfb
RobertoUnipd18 <roberto.vicentini@studenti.unipd.it>
2018-11-11 21:15:01 +0100

    Branch G4 CommonDemo
    Branch Group 4 CommonDemo

---
+++ app/src/test/java/unipd/se18/ocrcamera/TextExtractorTest.java
@@ -0,0 +1,12 @@
+package unipd.se18.ocrcamera;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextExtractorTest {
+
+    @Test
+    void getTextFromImg() {
+    }
+}
