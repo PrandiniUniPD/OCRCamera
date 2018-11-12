b8fd780026175bac9dc0d37824b8ebad7209b5cb
buffyeah <prandinip@gmail.com>
2018-11-12 17:12:41 +0100

    working on TestResultActivity

--- app/src/main/java/unipd/se18/ocrcamera/TestResultActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/TestResultActivity.java
@@ -6,19 +6,12 @@ import android.support.v7.widget.RecyclerView;
 
 public class TestResultActivity extends AppCompatActivity {
 
-    /**
-     * The path where the database is saved
-     */
-    private final String dirPath = ""; //TODO the dirpath where the database is saved
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_result);
 
-        PhotoTester tester = new PhotoTester(dirPath);
-
         RecyclerView results = findViewById(R.id.test_entries_list);
-        //results.setAdapter(new AdapterTestEntry()); //TODO
+        //results.setAdapter(new AdapterTestEntry(this, JSONReportParser.parseReport(stringofJSONContent))); //TODO
     }
 }
