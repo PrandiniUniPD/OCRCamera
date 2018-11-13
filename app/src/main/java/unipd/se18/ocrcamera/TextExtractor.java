503f2d072cf15914f4083456227918aaca4bb451
lucamoro <lucamoro96@gmail.com>
2018-11-13 23:14:46 +0100

    added CountDownLatch to wait for the text extractor to finish

--- app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
+++ app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
@@ -10,6 +10,8 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Class the implements the common OCR interface to retrieve text from an image
  * @author Pietro Prandini (g2)
@@ -51,17 +53,30 @@ public class TextExtractor implements OCRInterface {
         FirebaseVisionImage fbImage = FirebaseVisionImage.fromBitmap(img);
         // Defines that will be used an on device text recognizer
         FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-        //
+
+
+        //latch used to wait for extraction to finish
+        final CountDownLatch latch = new CountDownLatch(1);
+
         Task<FirebaseVisionText>fbText = textRecognizer.processImage(fbImage)
                 .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
             @Override
             public void onSuccess(FirebaseVisionText firebaseVisionText) {
                 Log.v(TAG, "extractText -> onSuccess ->\n-----      RECOGNIZED TEXT       -----\n"
                         + firebaseVisionText.getText() + "\n----- END OF THE RECOGNIZED TEXT -----");
+                latch.countDown();
             }
         });
-        // Wait until the end of the task
-        while (!fbText.isSuccessful()); // TODO optimization of this statement
+
+        if(!fbText.isSuccessful()) {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                return "Failed to extract text.";
+            }
+
+        }
+
         long afterWaiting = java.lang.System.currentTimeMillis();
         Log.v(TAG, "extractText -> text extracted in " + (afterWaiting - beforeWaiting) + " milliseconds");
         // Return the recognized text
