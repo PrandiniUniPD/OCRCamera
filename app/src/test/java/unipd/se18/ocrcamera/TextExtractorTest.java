53d2924f856ccee6b60167473c46aa88ac6d5c12
Andrea Ton <andrea97at@gmail.com>
2018-11-13 01:07:15 +0100

    Added getTextFromImg() {TRIAL} test method
    Unit test of method getTextFromImg().

--- app/src/test/java/unipd/se18/ocrcamera/TextExtractorTest.java
+++ app/src/test/java/unipd/se18/ocrcamera/TextExtractorTest.java
@@ -1,12 +1,32 @@
 package unipd.se18.ocrcamera;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TextExtractorTest {
+/*
+@author Elia Bedin, Andrea Ton
+*/
+
+class TextExtractorTest extends TextExtractor{
+
+    TextExtractorTest(Context context) {
+        super(context);
+    }
 
     @Test
-    void getTextFromImg() {
+    void getTextFromImg_Empty() {
+      Bitmap IMG_Generated = null;
+      try {
+          IMG_Generated = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+      } catch (Exception e) {
+        e.getCause().printStackTrace();
+      }
+      String result = getTextFromImg(IMG_Generated);
+      //assertNotNull(result);
+      assertEquals(result, "No text retrieved");
     }
 }
