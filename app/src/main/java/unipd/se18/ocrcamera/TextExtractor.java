91acd388efa7c1c2493c5b6d868a3ef738f2ae3c
buffyeah <prandinip@gmail.com>
2018-11-24 12:44:44 +0100

    sorting from top to bottom

--- app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
+++ app/src/main/java/unipd/se18/ocrcamera/TextExtractor.java
@@ -10,6 +10,10 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -23,72 +27,123 @@ public class TextExtractor implements OCRInterface {
     private final String TAG = "TextExtractor";
 
     /**
-     * Constructor
+     * Number to indicate the point on the top-left position of an Object
      */
-    TextExtractor() {    }
+    private final int TOP_LEFT = 0;
+
+    /**
+     * Number to indicate the point on the top-right position of an Object
+     */
+    private final int TOP_RIGHT = 1;
+
+    /**
+     * Number to indicate the point on the bottom-left position of an Object
+     */
+    private final int BOTTOM_LEFT = 2;
+
+    /**
+     * Number to indicate the point on the bottom-right position of an Object
+     */
+    private final int BOTTOM_RIGHT = 3;
 
     /**
      * Extracts a text from a given image.
      *
      * @param img The image in a Bitmap format
-     * @return The String of the text recognized (empty String if nothing is recognized)
+     * @return The String of the ingredients recognized (empty String if nothing is recognized)
      * @author Pietro Prandini (g2)
      */
-    public String getTextFromImg(Bitmap img)
-    {
+    public String getTextFromImg(Bitmap img) {
         Log.d(TAG, "getTextFromImg");
-        return extractText(img);
+        FirebaseVisionText textExtracted = extractFireBaseVisionText(img);
+        if (textExtracted != null) {
+            StringBuilder text = new StringBuilder();
+            for(FirebaseVisionText.TextBlock block: sortBlocks(textExtracted)) {
+                text.append(block.getText()).append("\n");
+            }
+            return text.toString();
+        } else {
+            return "";
+        }
     }
 
     /**
      * Extracts a text from a given image.
      *
      * @param img The image in a Bitmap format
-     * @return The String of the text recognized (empty String if nothing is recognized)
-     * @author Pietro Prandini (g2)
+     * @return The FirebaseVisionText of the text recognized, null if nothing is recognized
+     * @author Pietro Prandini (g2), Luca Moroldo (g3)
      */
-    private String extractText(Bitmap img)
-    {
+    private FirebaseVisionText extractFireBaseVisionText(Bitmap img) {
         Log.d(TAG, "extractText");
         long beforeWaiting = java.lang.System.currentTimeMillis();
         // Defines the image that will be analysed to get the text
         FirebaseVisionImage fbImage = FirebaseVisionImage.fromBitmap(img);
         // Defines that will be used an on device text recognizer
-        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-
+        FirebaseVisionTextRecognizer textRecognizer =
+                FirebaseVision.getInstance().getOnDeviceTextRecognizer();
 
         //latch used to wait for extraction to finish
         final CountDownLatch latch = new CountDownLatch(1);
 
-        Task<FirebaseVisionText>fbText = textRecognizer.processImage(fbImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+        Task<FirebaseVisionText> fbText = textRecognizer.processImage(fbImage).addOnSuccessListener(
+                new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        Log.v(TAG, "extractText -> onSuccess ->\n"
+                                + "-----      RECOGNIZED TEXT       -----"
+                                + "\n" + firebaseVisionText.getText() + "\n"
+                                + "----- END OF THE RECOGNIZED TEXT -----");
+                        //analogous to signal
+                        latch.countDown();
+                    }
+                });
 
-                Log.v(TAG, "extractText -> onSuccess ->\n-----      RECOGNIZED TEXT       -----\n"
-                        + firebaseVisionText.getText() + "\n----- END OF THE RECOGNIZED TEXT -----");
-
-                //analogous to signal
-                latch.countDown();
-            }
-        });
-
-        if(!fbText.isSuccessful()) {
-            try
-            {
+        if (!fbText.isSuccessful()) {
+            try {
                 //analogous to wait
                 latch.await();
-            }
-            catch (InterruptedException e)
-            {
-                return "Failed to extract text.";
+            } catch (InterruptedException e) {
+                Log.e(TAG, "Nothing recognized");
+                return null;
             }
 
         }
 
         long afterWaiting = java.lang.System.currentTimeMillis();
-        Log.i(TAG, "extractText -> text extracted in " + (afterWaiting - beforeWaiting) + " milliseconds");
+        Log.i(TAG, "extractText -> text extracted in "
+                + (afterWaiting - beforeWaiting) + " milliseconds");
         // Return the recognized text
-        String ocrResult = fbText.getResult().getText();
-        return ocrResult;
+        return fbText.getResult();
+    }
+    /**
+     * Sort the blocks recognized
+     * @param OCRResult FirebaseVisionText object produced by an OCR recognition
+     * @return An ArrayList of FirebaseVisionText sorted
+     */
+    private ArrayList<FirebaseVisionText.TextBlock> sortBlocks(FirebaseVisionText OCRResult) {
+        ArrayList<FirebaseVisionText.TextBlock> OCRBlocks = new ArrayList<>(OCRResult.getTextBlocks());
+        OCRBlocks = sortBlocksY(OCRBlocks);
+        return OCRBlocks;
+    }
+
+    /**
+     * Sort the blocks recognized from top to bottom
+     * @param OCRBlocks ArrayList of FirebaseVisionText.TextBlock recognized by the OCR processing
+     * @return An ArrayList of FirebaseVisionText sorted from top to bottom
+     * @author Pietro Prandini (g2)
+     */
+    private ArrayList<FirebaseVisionText.TextBlock> sortBlocksY(ArrayList<FirebaseVisionText.TextBlock> OCRBlocks) {
+        // Comparator for ordering the blocks by the y axis
+        Comparator<FirebaseVisionText.TextBlock> mYComparator = new Comparator<FirebaseVisionText.TextBlock>() {
+            @Override
+            public int compare(FirebaseVisionText.TextBlock o1, FirebaseVisionText.TextBlock o2) {
+                int o1TopLeftY = Objects.requireNonNull(o1.getCornerPoints())[TOP_LEFT].y;
+                int o2TopLeftY = Objects.requireNonNull(o2.getCornerPoints())[TOP_LEFT].y;
+                return Integer.compare(o1TopLeftY,o2TopLeftY);
+            }
+        };
+        Collections.sort(OCRBlocks, mYComparator);
+        return OCRBlocks;
     }
 }
