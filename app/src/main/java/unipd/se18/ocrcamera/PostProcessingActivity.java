9d88ce4b02bcd23ec189aaf7b3ebe1b68f792a8a
Giovanni Furlan <giovanni.furlan.4@studenti.unipd.it>
2018-11-25 12:09:41 +0100

    IN WORKING
    in working

--- app/src/main/java/unipd/se18/ocrcamera/PostProcessingActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/PostProcessingActivity.java
@@ -27,7 +27,6 @@ import java.net.URI;
 public class PostProcessingActivity extends AppCompatActivity
 {
 
-    private UCropView cropView;
     private Button btnConfirmCrop;
 
     @Override
@@ -36,28 +35,24 @@ public class PostProcessingActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_processing);
 
+        //UI components initialization
+        btnConfirmCrop = findViewById(R.id.btnConfirmCrop);
+
         //Get intent extra
         String filePath = getIntent().getStringExtra("imgUri");
 
+        //Build Uri from filePath adding scheme
         Uri.Builder builder = new Uri.Builder().scheme("file").path(filePath);
         final Uri captureImageUri = builder.build();
 
-        //Cropview initialization
-        cropView = findViewById(R.id.cropView);
-        cropView.getCropImageView().setImageURI(captureImageUri);
-        cropView.getOverlayView().setShowCropFrame(true);
-        cropView.getOverlayView().setFreestyleCropMode(OverlayView.FREESTYLE_CROP_MODE_ENABLE);
-
-        //UI components initialization
-        btnConfirmCrop = findViewById(R.id.btnConfirmCrop);
+        //Create a new result file and take his Uri
+        final Uri resultImageUri = Uri.fromFile(new File("/data/user/0/unipd.se18.ocrcamera/cache/AFile.jpg"));
+        UCrop.of(captureImageUri, resultImageUri).start(PostProcessingActivity.this);
 
         btnConfirmCrop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                final Uri resultImageUri = Uri.fromFile(new File("/data/user/0/unipd.se18.ocrcamera/cache/AFile.jpg"));
-                UCrop.of(captureImageUri, resultImageUri).start(PostProcessingActivity.this);
-
+                //Raplace image of Uri with cropped one
                 SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
                 SharedPreferences.Editor edit = prefs.edit();
                 edit.putString("imagePath", resultImageUri.getPath());
@@ -76,5 +71,14 @@ public class PostProcessingActivity extends AppCompatActivity
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.crop_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
     }
 }
