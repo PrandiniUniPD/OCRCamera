efdda80c2d28b819f914d81f8ae5020784aecfbd
leopra <44270170+leopra@users.noreply.github.com>
2018-11-26 21:30:54 +0100

    Commit su test da rivedere
    Ho aggiunto solo una classe test non ho modificato niente, ormai stavo lavorando sul tuo branch.

---
+++ app/src/test/java/unipd/se18/ocrcamera/TestTest.java
@@ -0,0 +1,20 @@
+package unipd.se18.ocrcamera;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+    @Test
+    public void rotationTest()
+    {
+        String path = this.getClass().getClassLoader().getResource("getTextFromFileTest.txt").getPath();
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        Bitmap bitmapKK = BitmapFactory.decodeFile(path,bmOptions);
+        Bitmap bitmaprotato = ResultActivity.rotateBitmap(bitmapKK, 0);
+        assertEquals(bitmapKK, bitmaprotato);
+    }
+}
