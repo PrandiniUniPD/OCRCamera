d04956ee316b8397337b9eb8b6e6cbd099a71b94
Giofasan <giovanni.fasan@studenti.unipd.it>
2018-11-30 11:42:20 +0100

    beta1
    Add two function for find inci's ingredients

--- app/src/main/java/unipd/se18/ocrcamera/ResultActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/ResultActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
@@ -26,8 +27,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -45,6 +53,11 @@ public class ResultActivity extends AppCompatActivity {
      */
     private Bitmap lastPhoto;
 
+    /**
+     * TextView per gli ingredienti
+     */
+    private TextView tViewInci;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +70,9 @@ public class ResultActivity extends AppCompatActivity {
         ImageView mImageView = findViewById(R.id.img_captured_view);
         mOCRTextView = findViewById(R.id.ocr_text_view);
         mOCRTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        tViewInci = findViewById(R.id.textViewInci);
+        tViewInci.setMovementMethod(new ScrollingMovementMethod());
 
         FloatingActionButton fab = findViewById(R.id.newPictureFab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -94,6 +110,7 @@ public class ResultActivity extends AppCompatActivity {
             } else {
                 //Show the text of the last image
                 mOCRTextView.setText(OCRText);
+                tViewInci.setText(Html.fromHtml("Ingredienti trovati: "+inciDetectorEtichetta(OCRText)+""));
             }
         } else{
             // text from OCR
@@ -160,6 +177,7 @@ public class ResultActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             mOCRTextView.setText(finalTextRecognized);
+                            tViewInci.setText(Html.fromHtml("Ingredienti trovati: "+inciDetectorEtichetta(finalTextRecognized)+""));
                         }
                     });
                 }
@@ -170,6 +188,7 @@ public class ResultActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             mOCRTextView.setText(finalTextRecognized);
+                            tViewInci.setText(Html.fromHtml("Ingredienti trovati: "+inciDetectorEtichetta(finalTextRecognized)+""));
                         }
                     });
                 }
@@ -211,6 +230,70 @@ public class ResultActivity extends AppCompatActivity {
         canvas.drawBitmap(original, 5.0f, 0.0f, null);
 
         return rotatedBitmap;
+    }
+
+    /**
+     * @author Giovanni Fasan(g1), Giovanni Piva(g1)
+     * @param text String in which you have to find the ingredients
+     * @return String with the list of ingredients found in the text
+     * Put in database csv: https://www.youtube.com/watch?v=i-TqNzUryn8
+     */
+    public String inciDetector(String text){
+        String inci = "";
+        try {
+            InputStream is = getResources().openRawResource(R.raw.database);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String ingredient;
+            while ((ingredient=reader.readLine())!=null){
+                if (text.toUpperCase().contains(ingredient)) {
+                    inci = inci + "<b>" + ingredient + "</b>; ";
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return inci;
+    }
+
+    /**
+     * @author Giovanni Fasan(g1), Giovanni Piva(g1)
+     * @param text String in which you have to find the ingredients
+     * @return String with ingredients list of the label
+     * Put in database csv: https://www.youtube.com/watch?v=i-TqNzUryn8
+     * problem contains
+     */
+    public String inciDetectorEtichetta(String text){
+        String inci = "";
+        List listInci = new ArrayList();
+        try {
+            InputStream is = getResources().openRawResource(R.raw.database);
+            is.mark(0);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String ingredient;
+            String noSpace = text.replaceAll("\n", " ");
+            String[] tokens = noSpace.split(",");
+            String box="";
+            for(int i=0; i<tokens.length; i++) {
+                while ((ingredient=reader.readLine())!=null){
+                    if (tokens[i].toUpperCase().contains(ingredient)) {
+                        box=ingredient;
+                    }
+                }
+                is.reset();
+                if(!listInci.contains(box)) {
+                    listInci.add(box);
+                }
+            }
+
+            for(int i=0; i<listInci.size(); i++){
+                inci = inci + "<b>" + listInci.get(i) + "</b>; ";
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return inci;
     }
 
 }
