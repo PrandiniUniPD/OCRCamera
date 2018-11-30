1b001797ad6470d62943614d6846d416a0242080
Francesco Pham <pham.francesco@gmail.com>
2018-11-30 22:07:38 +0100

    Some performance improvements

--- app/src/main/java/unipd/se18/ocrcamera/BrightnessRecognition.java
+++ app/src/main/java/unipd/se18/ocrcamera/BrightnessRecognition.java
@@ -22,7 +22,7 @@ public class BrightnessRecognition {
      * Constructor of a BrightnessRecognition object containing a Bitmap
      * @param bmp the bitmap of the image to calculate the brightness of
      * @modify image with the new bitmap
-     * @author Pietro Balzan
+     * @author Pietro Balzan - some changes by Francesco Pham
      */
 
     public BrightnessRecognition(Bitmap bmp, Context c, Handler h){
@@ -34,18 +34,22 @@ public class BrightnessRecognition {
     /**
      * This method calculates the brightness of the image and prompts a Toast to the user
      * @author Pietro Balzan
+     * @param pixelSkip how many pixels to skip each pixel. Higher values result in better performance,
+     *                     but a more rough estimate. When pixelSpacing = 1, the method actually
+     *                     calculates the real average brightness, not an estimate.
+     *                     Do not use values for pixelSpacing that are smaller than 1.
      */
-    public void imgBrightness(){
+    public void imgBrightness(int pixelSkip){
         int width = image.getWidth();
         int height = image.getHeight();
         int R, G, B, pixel;
-        int total_pixels = width*height;
+        int totalPixels = 0;
         int UpperBound= 190;
         int LowerBound= 80;
         double tot= 0;
 
-        for(int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
+        for(int x = 0; x < width; x+=pixelSkip) {
+            for (int y = 0; y < height; y+=pixelSkip) {
                 // get pixel color
                 pixel = image.getPixel(x, y);
                 R = Color.red(pixel);
@@ -53,19 +57,20 @@ public class BrightnessRecognition {
                 B = Color.blue(pixel);
                 double brightness = (0.2126 * R + 0.7152 * G + 0.0722 * B); //  RGB/Luma conversion formula, dermines luminance of a pixel
                 tot+= brightness;
+                totalPixels++;
             }
         }
 
-        final String toobright = "The picture might be too bright";
-        final String toodark = "The picture might be too dark";
-        double media= tot/total_pixels;
+        final String tooBright = "The picture might be too bright";
+        final String tooDark = "The picture might be too dark";
+        double media= tot/totalPixels;
 
         if (media> UpperBound){
             Log.d(TAG, "too bright image");
             handler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(appContext, toobright, Toast.LENGTH_LONG).show();
+                    Toast.makeText(appContext, tooBright, Toast.LENGTH_LONG).show();
                 }
             });
             // image is too bright
@@ -75,7 +80,7 @@ public class BrightnessRecognition {
             handler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(appContext, toodark, Toast.LENGTH_LONG).show();
+                    Toast.makeText(appContext, tooDark, Toast.LENGTH_LONG).show();
                 }
             });
             //image is too dark
