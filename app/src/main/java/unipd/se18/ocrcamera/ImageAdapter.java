213f9e7a349b29b44a50e08c267ab91d97852521
leopra <44270170+leopra@users.noreply.github.com>
2018-12-09 16:59:56 +0100

    Galleria Gridview con ImageAdapter (non più int[] da folder Drawable)

---
+++ app/src/main/java/unipd/se18/ocrcamera/ImageAdapter.java
@@ -0,0 +1,55 @@
+package unipd.se18.ocrcamera;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Bitmap> bitmapList;
+
+    public ImageAdapter(Context c, ArrayList<Bitmap> bitmapList) {
+        this.context = c;
+        this.bitmapList= bitmapList;
+    }
+
+    // returns the number of images
+    public int getCount() {
+        return bitmapList.size();
+    }
+
+    // returns the ID of an item
+    public Object getItem(int position) {
+        return position;
+    }
+
+    // returns the ID of an item
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // returns an ImageView view
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // create a ImageView programmatically
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(this.context);
+            imageView.setLayoutParams(new GridView.LayoutParams(115,115));
+            imageView.setScaleType((ImageView.ScaleType.CENTER_CROP));
+        }
+        else {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageBitmap(this.bitmapList.get(position));
+        return imageView;
+    }
+}
--- app/src/main/java/unipd/se18/ocrcamera/ImageLibraryActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/ImageLibraryActivity.java
@@ -4,14 +4,20 @@ package unipd.se18.ocrcamera;
 
 
 
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Gallery;
+import android.widget.GridView;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 /** Classe che implementa una galleria di immagini
  * Leonardo Pratesi - gruppo 1
@@ -21,33 +27,33 @@ import android.widget.ImageView;
 
     public class ImageLibraryActivity extends AppCompatActivity {
 
-        Gallery simpleGallery;
-        CustomGalleryAdapter customGalleryAdapter;
-        ImageView selectedImageView;
-        // array of images
-        int[] images = {R.drawable.image1, //R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5,
-                //R.drawable.image6, R.drawable.image7, R.drawable.image8, R.drawable.image9, R.drawable.image10, R.drawable.image11,
-                //R.drawable.image12, R.drawable.image13
-        };
+    private GridView imageGrid;
+    private ArrayList<Bitmap> bitmapList;
+    // array of images
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.imagegallery);
-            simpleGallery = (Gallery) findViewById(R.id.simpleGallery); // get the reference of Gallery
-            selectedImageView = (ImageView) findViewById(R.id.selectedImageView); // get the reference of ImageView
-            customGalleryAdapter = new CustomGalleryAdapter(getApplicationContext(), images); // initialize the adapter
-            simpleGallery.setAdapter(customGalleryAdapter); // set the adapter
-            simpleGallery.setSpacing(10);
-            // perform setOnItemClickListener event on the Gallery
-            simpleGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    // set the selected image in the ImageView
-                    selectedImageView.setImageResource(images[position]);
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.imagegallery);
+        this.imageGrid = (GridView) findViewById(R.id.griglia);
+        this.bitmapList = new ArrayList<Bitmap>();
+
+        File path = new File(Environment.getExternalStorageDirectory(), "iWallet/Images");
+        if (path.exists()) {
+            String[] fileNames = path.list();
+            try {
+                for (int i = 0; i < fileNames.length; i++) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(path.getPath() + "/" + fileNames[i]);
+                    this.bitmapList.add(bitmap);
                 }
-            });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
+        this.imageGrid.setAdapter(new ImageAdapter(this, this.bitmapList));
     }
+}
+
 
