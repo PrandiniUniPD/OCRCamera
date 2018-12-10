4883123ed44d137181ca99a2bca0be3aec9b1eee
Unknown <stefano.romanello@hotmail.it>
2018-12-10 22:22:43 +0100

    Fixed permission request

--- app/src/main/java/unipd/se18/ocrcamera/DownloadDbActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/DownloadDbActivity.java
@@ -1,9 +1,13 @@
 package unipd.se18.ocrcamera;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,6 +34,7 @@ public class DownloadDbActivity extends AppCompatActivity {
     private Button clickButtonLogin;
     private final String LOGINGINFORMATION_FILE = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)+"/ingsoftwareftp.txt";
     private final String PHOTOS_FOLDER = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+"/OCRCameraDB";
+    private final int REQUEST_PERMISSION_CODE = 500;
     private LinearLayout layoutLogin;
     private LinearLayout layoutDownload;
 
@@ -37,7 +42,9 @@ public class DownloadDbActivity extends AppCompatActivity {
     private EditText txtUsrname;
     private EditText txtPassword;
 
+
     private TextView txtInternetStatus;
+    private TextView txtPermissionStatus;
 
     /**
      * Instantiate the UI elements and check if is possible to do the login.
@@ -55,26 +62,36 @@ public class DownloadDbActivity extends AppCompatActivity {
         txtPassword = (EditText) findViewById(R.id.txtPasswordDownload);
         txtUsrname = (EditText) findViewById(R.id.txtUsernameDownload);
         txtInternetStatus = (TextView) findViewById(R.id.txtInternetStatusDownload);
+        txtPermissionStatus = (TextView) findViewById(R.id.txtPermissionStatusDownload);
         
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
+        //Check and in case Ask for permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_CODE);
+        }
 
         //Verify if the login is already done and if there is internet connection
         final File file = new File(LOGINGINFORMATION_FILE);
-        if (!file.exists() && cm.getActiveNetworkInfo() != null) {
-           //No file, have to do the login
-            layoutLogin.setVisibility(View.VISIBLE);
-        }
-        else if(file.exists() && cm.getActiveNetworkInfo() != null)
-        {
-            //Can do the login
-            layoutDownload.setVisibility(View.VISIBLE);
-        }
-        else if(cm.getActiveNetworkInfo() == null)
-        {
 
+        if(cm.getActiveNetworkInfo() == null) //No internet
+        {
             txtInternetStatus.setVisibility(View.VISIBLE);
         }
+        else if (!file.exists() && cm.getActiveNetworkInfo() != null) //No file, have to do the login
+        {
+
+            layoutLogin.setVisibility(View.VISIBLE);
+        }
+        else if(file.exists() && cm.getActiveNetworkInfo() != null) //Can do the login
+        {
+            layoutDownload.setVisibility(View.VISIBLE);
+        }
+
+
+
 
         ///Load other UI elements
         clickButtonDownload = (Button) findViewById(R.id.downloadDbButton);
@@ -136,6 +153,31 @@ public class DownloadDbActivity extends AppCompatActivity {
         layoutDownload.setVisibility(View.VISIBLE);
         layoutLogin.setVisibility(View.GONE);
 
+    }
+    /**
+     * Verify if the user granted the permission
+     *
+     * @author Stefano Romanello (g3)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    txtPermissionStatus.setVisibility(View.GONE);
+
+                } else {
+                    // permission denied
+                    txtPermissionStatus.setVisibility(View.VISIBLE);
+                    layoutLogin.setVisibility(View.GONE);
+                    layoutDownload.setVisibility(View.GONE);
+                }
+                return;
+            }
+        }
     }
 
 
