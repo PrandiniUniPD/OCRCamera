36baf6a73774162ea56c35e34dd9b804c78128dd
leopra <44270170+leopra@users.noreply.github.com>
2018-12-16 16:50:24 +0100

    Trying to learn Asynchronous methods 2 (private thread in imagelibraryactivity

--- app/src/main/java/unipd/se18/ocrcamera/ImageLibraryActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/ImageLibraryActivity.java
@@ -6,6 +6,7 @@ package unipd.se18.ocrcamera;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.Gallery;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,9 +33,9 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
     public class ImageLibraryActivity extends AppCompatActivity {
 
     private GridView imageGrid;
-    private ArrayList<Bitmap> bitmapList;
+    private ArrayList<Bitmap> bitmapList; //Array of images
     private final String PHOTOS_FOLDER = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+"/OCRCameraDB";
-    // array of images
+
 
 
     @Override
@@ -43,35 +45,85 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
         setContentView(R.layout.imagegallery);
         this.imageGrid = (GridView) findViewById(R.id.griglia);
         this.bitmapList = new ArrayList<Bitmap>();
-
-        /** File path = new File(PHOTOS_FOLDER);
-        System.out.println(path);
-        System.out.println(path.list()); //il path non contiene elementi =null RISOLVERE
+        imageGrid.setAdapter(new ImageAdapter(this, bitmapList));
 
 
-         if (path.exists()) {
-            String[] fileNames = path.list();
+        // listener per interagire con le foto
+        imageGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(ImageLibraryActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
+        public void start(View v)
+        {
+            new BackgroundTask().execute();
+        }
+    private class BackgroundTask extends AsyncTask<Void, Integer, String>
+    {
+        public int photoloaded= 0;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(Void... arg0)
+        {
+            File path = new File(PHOTOS_FOLDER);
+            if (path.exists()) {
+                String[] fileNames = path.list();
                 for (int i = 0; i < 20; i++) {
-                    System.out.println(fileNames[i]); //controllare che file carica
-
-
-                     works but very slow, create Thread?
                     try {
-                        File f=new File(PHOTOS_FOLDER, fileNames[i]);
+                        File f = new File(PHOTOS_FOLDER, fileNames[i]);
                         bitmapList.add(BitmapFactory.decodeStream(new FileInputStream(f)));
                     }
                     catch (FileNotFoundException e)
                     {
                         e.printStackTrace();
                     }
+            }
 
-                    */
-                    new Thread(new GalleryThread(PHOTOS_FOLDER, bitmapList, imageGrid));
+        }
+            //imageGrid.setAdapter(new ImageAdapter(this, bitmapList));
+            return "Lavoro Terminato!";
+        }
+
+        protected void onProgressUpdate(Integer... values)
+        {
 
 
+        }
 
-                    //non funziona
+
+        /** File path = new File(PHOTOS_FOLDER);
+         System.out.println(path);
+         System.out.println(path.list()); //il path non contiene elementi =null RISOLVERE
+
+
+         if (path.exists()) {
+         String[] fileNames = path.list();
+
+         for (int i = 0; i < 20; i++) {
+         System.out.println(fileNames[i]); //controllare che file carica
+
+
+         works but very slow, create Thread?
+         try {
+         File f=new File(PHOTOS_FOLDER, fileNames[i]);
+         bitmapList.add(BitmapFactory.decodeStream(new FileInputStream(f)));
+         }
+         catch (FileNotFoundException e)
+         {
+         e.printStackTrace();
+         }
+
+         */
+
+        //non funziona
                     //InternalStorageManager dir = new InternalStorageManager(this, PHOTOS_FOLDER, fileNames[i]);
                     //bitmapList.add(dir.loadBitmapFromInternalStorage());
 
