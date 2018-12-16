a9de648d43c69e814c84d59d306fbf3e9ad0b236
leopra <44270170+leopra@users.noreply.github.com>
2018-12-16 17:29:22 +0100

    Trying to learn Asynchronous methods (working but setAdapter loads the arraylist when it is still empty)

--- app/src/main/java/unipd/se18/ocrcamera/ImageLibraryActivity.java
+++ app/src/main/java/unipd/se18/ocrcamera/ImageLibraryActivity.java
@@ -34,9 +34,8 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
 
     private GridView imageGrid;
     private ArrayList<Bitmap> bitmapList; //Array of images
-    private final String PHOTOS_FOLDER = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+"/OCRCameraDB";
-
-
+    private final String PHOTOS_FOLDER = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/OCRCameraDB";
+    private int progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +44,6 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
         setContentView(R.layout.imagegallery);
         this.imageGrid = (GridView) findViewById(R.id.griglia);
         this.bitmapList = new ArrayList<Bitmap>();
-        imageGrid.setAdapter(new ImageAdapter(this, bitmapList));
 
 
         // listener per interagire con le foto
@@ -56,47 +54,41 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
                         Toast.LENGTH_SHORT).show();
             }
         });
+    // thread che carica immagini in bitmapList
+        new Thread() {
+            public void run()
+            {
+                try{
+                File path = new File(PHOTOS_FOLDER);
+                if (path.exists()) {
+                    String[] fileNames = path.list();
+                    for (int i = 0; i < 60; i++) {
+                        try {
+                            File f = new File(PHOTOS_FOLDER, fileNames[i]);
+                            bitmapList.add(BitmapFactory.decodeStream(new FileInputStream(f)));
+                            progress++;
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+        if (progress==20)
+        {
+            imageGrid.setAdapter(new ImageAdapter(this, bitmapList));
+        }
     }
 
-        public void start(View v)
-        {
-            new BackgroundTask().execute();
-        }
-    private class BackgroundTask extends AsyncTask<Void, Integer, String>
-    {
-        public int photoloaded= 0;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-        @Override
-        protected String doInBackground(Void... arg0)
-        {
-            File path = new File(PHOTOS_FOLDER);
-            if (path.exists()) {
-                String[] fileNames = path.list();
-                for (int i = 0; i < 20; i++) {
-                    try {
-                        File f = new File(PHOTOS_FOLDER, fileNames[i]);
-                        bitmapList.add(BitmapFactory.decodeStream(new FileInputStream(f)));
-                    }
-                    catch (FileNotFoundException e)
-                    {
-                        e.printStackTrace();
-                    }
-            }
-
-        }
-            //imageGrid.setAdapter(new ImageAdapter(this, bitmapList));
-            return "Lavoro Terminato!";
-        }
-
-        protected void onProgressUpdate(Integer... values)
-        {
+}
 
 
-        }
 
 
         /** File path = new File(PHOTOS_FOLDER);
@@ -127,9 +119,9 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
                     //InternalStorageManager dir = new InternalStorageManager(this, PHOTOS_FOLDER, fileNames[i]);
                     //bitmapList.add(dir.loadBitmapFromInternalStorage());
 
-                }
 
-        }
+
+
         //this.imageGrid.setAdapter(new ImageAdapter(this, this.bitmapList));
 
 
