990813ad90ae879349fabd9a46bfddc0a8f4377b
Pietro <pietro.balzan@studenti.unipd.it>
2018-12-22 12:11:35 +0100

    Specification  added to AllergensManager

--- app/src/main/java/unipd/se18/ocrcamera/AllergensManager.java
+++ app/src/main/java/unipd/se18/ocrcamera/AllergensManager.java
@@ -3,6 +3,9 @@ package unipd.se18.ocrcamera;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+
+import unipd.se18.ocrcamera.inci.IngredientsExtractor;
 
 
 /**
@@ -11,17 +14,32 @@ import java.util.ArrayList;
  */
 class AllergensManager {
 
-    static ArrayList<Allergen> listAllergens = new ArrayList<>();
+    static ArrayList<Allergen> allergensList = new ArrayList<>();
 
-    public static ArrayList<Allergen> getListAllergens(InputStream allergendbStream) {
-        InputStreamReader allergendbReader = new InputStreamReader(allergendbStream);
-
-
-        return listAllergens;
+    /**
+     * This method returns an list of Allergen objects from the input file
+     * @param allergenDbStream, an InputStream containing the list of allergens from a file
+     * @return allergensList, the converted ArrayList of Allergen objects
+     * @modify allergensList by putting all allergens in it
+     * @author Cervo Nicolò
+     * @author Balzan Pietro
+     */
+    public static ArrayList<Allergen> getAllergensList(InputStream allergenDbStream) {
+        InputStreamReader allergenDbReader = new InputStreamReader(allergenDbStream);
+        return allergensList;
     }
 
-    public static String[] matchAllergenesWithRecognizedIgredients() {
-        String[] i = new String[2];
-        return i;
+    /**
+     * this method compares the list of allergens to a String, checking if there are
+     * some matching ingredients
+     * @param ingredients a List of the ingredients (recognized form the oCR text)
+     *                    to be compared to the allergens
+     * @return allergensInText, ArrayList of the matching ingredients
+     * @author Cervo Nicolò
+     * @author Balzan Pietro
+     */
+    public static ArrayList<Allergen> findListAllergens(List ingredients) {
+        ArrayList<Allergen> allergensInText= new ArrayList<Allergen>();
+        return allergensInText;
     }
 }
