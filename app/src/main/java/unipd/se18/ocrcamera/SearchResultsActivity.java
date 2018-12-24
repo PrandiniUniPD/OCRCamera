c9056feda0a02d0190e742379955eb8b6579b881
lucamoro <lucamoro96@gmail.com>
2018-12-24 16:20:21 +0100

    created searchable item and search activity

---
+++ app/src/main/java/unipd/se18/ocrcamera/SearchResultsActivity.java
@@ -0,0 +1,13 @@
+package unipd.se18.ocrcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
