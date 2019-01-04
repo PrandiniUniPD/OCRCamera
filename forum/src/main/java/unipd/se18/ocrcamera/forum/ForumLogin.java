ef4f5053f9619a424833752928efcbe3e80f45ae
AlbVal97 <alberto.valente.3@studenti.unipd.it>
2019-01-05 00:21:26 +0100

    Made the fragment ForumLogin interact with its viewmodel Login_VM

--- forum/src/main/java/unipd/se18/ocrcamera/forum/ForumLogin.java
+++ forum/src/main/java/unipd/se18/ocrcamera/forum/ForumLogin.java
@@ -1,5 +1,6 @@
 package unipd.se18.ocrcamera.forum;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
@@ -23,9 +24,14 @@ import unipd.se18.ocrcamera.forum.viewmodels.Login_VM;
 public class ForumLogin extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
     private EditText emailEditText;
     private EditText pwdEditText;
     private Button loginButton;
+
+    String userEmail;
+    String userPwd;
+
     private Login_VM viewModel;
 
     public ForumLogin() {
@@ -53,6 +59,19 @@ public class ForumLogin extends Fragment {
         emailEditText = view.findViewById(R.id.eMailEditText);
         pwdEditText = view.findViewById(R.id.pwdEditText);
         loginButton = view.findViewById(R.id.loginButton);
+
+        //When the login button is clicked a request is sent through the viewmodel
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //The email and password inserted are gathered from the EditText objects
+                userEmail = emailEditText.getText().toString();
+                userPwd = pwdEditText.getText().toString();
+
+                //Then the credentials are handed to the viewmodel method to be checked
+                viewModel.loginToForum(getContext(), userEmail, userPwd);
+            }
+        });
     }
 
     public interface OnFragmentInteractionListener
--- forum/src/main/java/unipd/se18/ocrcamera/forum/viewmodels/Login_VM.java
+++ forum/src/main/java/unipd/se18/ocrcamera/forum/viewmodels/Login_VM.java
@@ -1,17 +1,24 @@
 package unipd.se18.ocrcamera.forum.viewmodels;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 import android.content.MutableContextWrapper;
+import android.util.Log;
 
 import java.util.ArrayList;
 
+import unipd.se18.ocrcamera.forum.R;
 import unipd.se18.ocrcamera.forum.RequestManager;
 
 public class Login_VM extends ViewModel implements LoginMethods {
 
+    public MutableLiveData<String> liveError = new MutableLiveData<>();
+
+    private final String LOG_TAG = "@@Login_VM";
+
     @Override
-    public void loginToForum(Context context, String username, String password) {
+    public void loginToForum(final Context context, String username, String password) {
 
         //Definition of the network request parameters
         ArrayList<RequestManager.Parameter> parameters = new ArrayList<>();
@@ -29,17 +36,18 @@ public class Login_VM extends ViewModel implements LoginMethods {
 
             @Override
             public void onConnectionFailed(String message) {
-
+                Log.d(LOG_TAG, message);
+                liveError.setValue(context.getString(R.string.loginFailedMessage));
             }
 
             @Override
             public void onParametersSendingFailed(String message) {
-
+                Log.d(LOG_TAG, message);
+                liveError.setValue(context.getString(R.string.loginFailedMessage));
             }
         });
 
-        //Sending of a netword request to check if the provided credentials are correct
-
+        //Sending a netword request to check if the provided credentials are correct
         manager.sendRequest(context, parameters);
     }
 }
