54f9a0bd6dd068f628e0abea5b0f11d64435e187
AlbVal97 <alberto.valente.3@studenti.unipd.it>
2019-01-04 17:18:48 +0100

    Set "Login_VM.java" viewmodel ready to be implemented

--- forum/src/main/java/unipd/se18/ocrcamera/forum/viewmodels/Login_VM.java
+++ forum/src/main/java/unipd/se18/ocrcamera/forum/viewmodels/Login_VM.java
@@ -1,13 +1,46 @@
 package unipd.se18.ocrcamera.forum.viewmodels;
 
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.content.MutableContextWrapper;
+
+import java.util.ArrayList;
+
+import unipd.se18.ocrcamera.forum.RequestManager;
 
 public class Login_VM extends ViewModel implements LoginMethods
 {
 
     @Override
-    public void loginToForum(String username, String password)
-    {
-        //TODO("Implements this method give a user the access to the forum contents")
+    public void loginToForum(String username, String password) {
+
+        //Definition of the network request parameters
+        ArrayList<RequestManager.Parameter> parameters = new ArrayList<>();
+        parameters.add(new RequestManager.Parameter("c", RequestManager.RequestType.LOGIN.value));
+
+        RequestManager manager = new RequestManager();
+
+        //Implementation of the RequestManager listener
+        manager.setOnRequestFinishedListener(new RequestManager.RequestManagerListener() {
+
+            @Override
+            public void onRequestFinished(String response) {
+
+            }
+
+            @Override
+            public void onConnectionFailed(String message) {
+
+            }
+
+            @Override
+            public void onParametersSendingFailed(String message) {
+
+            }
+        });
+
+        //Sending of a netword request to check if the provided credentials are correct
+
+        // manager.sendRequest(context, parameters);
     }
 }
