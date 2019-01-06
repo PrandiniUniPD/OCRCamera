cef84d6d8a1268c94c843aa7a4a7a4394d8bb18d
Unknown <giovanni.furlan.4@studenti.unipd.it>
2019-01-06 21:03:06 +0100

    Add Listener in AddPhoto
    add listener in addPhoto fragment

--- forum/src/main/java/unipd/se18/ocrcamera/forum/AddPost.java
+++ forum/src/main/java/unipd/se18/ocrcamera/forum/AddPost.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import unipd.se18.ocrcamera.forum.viewmodels.AddPost_VM;
 
@@ -33,6 +35,18 @@ public class AddPost extends Fragment {
     private EditText messageEditText;
     private String titleText;
     private String messageText;
+    private String username;
+    private String Tag;
+
+    /**
+     * Error messages
+     */
+    private final String messageNullCamps ="Titolo o messaggio non inseriti, riprovare";
+    private final String messagePostAdded ="Post inserito correttamente";
+    private final String messagePostNotAdded ="Post non inserito, riprovare";
+    private final String messageWrongParameters ="Parametri non corretti, riprovare";
+    private final String messageWrongJSON ="Errore nella creazione del JSON";
+
 
 
     public AddPost()
@@ -61,10 +75,103 @@ public class AddPost extends Fragment {
         {
             public void onClick(View view)
             {
-                titleText = titleEditText.getText().toString();
-                messageText = messageEditText.getText().toString();
+                titleText =  titleEditText.getText().toString();
+                messageText =  messageEditText.getText().toString();
+                username = getArguments().getString("username");
 
-                viewModel.addPostToForum(titleText,messageText);
+                if(messageText==null || titleText==null)
+                {
+                    Toast.makeText(getActivity(), messageNullCamps, Toast.LENGTH_LONG).show();
+
+                    //Restart fragment
+                    Fragment addPostFragment = new AddPost();
+                    getActivity()
+                            .getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(
+                                    R.id.fragmentContainer,
+                                    addPostFragment
+                            )
+                            .addToBackStack(null)
+                            .commit();
+
+                }
+                else{
+                    viewModel.addPostToForum(view.getContext(), titleText,messageText,username);
+                }
+
+                /**
+                 * Listener of AddPost_VM
+                 */
+                viewModel.setAddPostListener(new AddPost_VM.addPostListener() {
+                    @Override
+                    public void onPostAdded(String response) {
+                        Toast.makeText(getActivity(), messagePostAdded, Toast.LENGTH_LONG).show();
+
+                        //Start showPost fragment
+                        Fragment showPostFragment = new ShowPosts();
+                        getActivity()
+                                .getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(
+                                        R.id.fragmentContainer,
+                                        showPostFragment
+                                )
+                                .addToBackStack(null)
+                                .commit();
+                    }
+
+                    @Override
+                    public void onConnectionFailed(String error) {
+                        Toast.makeText(getActivity(), messagePostNotAdded, Toast.LENGTH_LONG).show();
+
+                        //Restart fragment
+                        Fragment addPostFragment = new AddPost();
+                        getActivity()
+                                .getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(
+                                        R.id.fragmentContainer,
+                                        addPostFragment
+                                )
+                                .addToBackStack(null)
+                                .commit();
+                    }
+
+                    @Override
+                    public void onParametersSendingFailed(String error) {
+                        Toast.makeText(getActivity(), messageWrongParameters, Toast.LENGTH_LONG).show();
+
+                        //Restart fragment
+                        Fragment addPostFragment = new AddPost();
+                        getActivity()
+                                .getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(
+                                        R.id.fragmentContainer,
+                                        addPostFragment
+                                )
+                                .addToBackStack(null)
+                                .commit();
+                    }
+
+                    @Override
+                    public void onJSONPostCreationFailed(String error) {
+                        Log.e(Tag, messageWrongJSON);
+
+                        //Restart fragment
+                        Fragment addPostFragment = new AddPost();
+                        getActivity()
+                                .getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(
+                                        R.id.fragmentContainer,
+                                        addPostFragment
+                                )
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                });
             }
         });
     }
