d06863bf07a72d160b29dfb61d23121257dc0f59
leopra <44270170+leopra@users.noreply.github.com>
2019-01-10 20:39:09 +0100

    Add files via upload

---
+++ app/src/main/java/unipd/se18/ocrcamera/BlurObject.java
@@ -0,0 +1,47 @@
+package unipd.se18.ocrcamera;
+
+import android.graphics.Bitmap;
+
+
+/** Class that holds the bitmap, the name , and the blur value calculated
+ *  Pratesi Leonardo
+ *
+ */
+
+public class BlurObject {
+    String name;
+    Bitmap image;
+    double blur;
+
+    public BlurObject(String name, Bitmap image, double blur)
+    {   this.name=name;
+        this.image= image;
+        this.blur=blur;
+    }
+
+    public BlurObject(String name, Bitmap image)
+    {
+        this.name=name;
+        this.image=image;
+        this.blur= CameraActivity.blurValue(this.image);
+    }
+
+    public Bitmap getImage()
+    {
+        return image;
+    }
+
+    public String getName() {return name;}
+
+    public double getBlur()
+    {
+        return blur;
+    }
+
+    public String toString()
+    {
+        return name + " " + Double.toString(blur);
+    }
+
+
+}
