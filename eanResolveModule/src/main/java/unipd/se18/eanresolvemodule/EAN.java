e048a478e46c1c840cfbf2fcc3b126edc51234a4
EliaBedin <44301074+EliaBedin@users.noreply.github.com>
2019-01-11 19:35:04 +0100

    Minor description error removed
    Removed a description error

--- eanResolveModule/src/main/java/unipd/se18/eanresolvemodule/EAN.java
+++ eanResolveModule/src/main/java/unipd/se18/eanresolvemodule/EAN.java
@@ -1,14 +1,13 @@
 package unipd.se18.eanresolvemodule;
 
 /**
- * Interface for EAN string resolve
+ * Wrapper interface for EAN string resolve
  * @author Elia Bedin
  */
 
 public interface EAN {
 
     /**
-     * Wrapper for OCR libraries. Extract a text from a given image.
      * @param EANCode the EAN value of a barcode
      * more at https://en.wikipedia.org/wiki/International_Article_Number
      * @return The string of the name and details of the product with that EAN value, if one is
