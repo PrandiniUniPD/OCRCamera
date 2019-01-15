8d6cd475882b0764f87ecc98fa9eee7cedd7613d
Andrea Ton <andrea.ton.2@studenti.unipd.it>
2019-01-15 23:21:07 +0100

    code refactored

--- barcodeModule/src/main/java/unipd/se18/barcodemodule/MLKitBarcode.java
+++ barcodeModule/src/main/java/unipd/se18/barcodemodule/MLKitBarcode.java
@@ -2,14 +2,12 @@ package unipd.se18.barcodemodule;
 
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -20,19 +18,10 @@ import java.util.concurrent.CountDownLatch;
  */
 public class MLKitBarcode implements Barcode{
 
-    //actual barcode String returned by main method decodeBarcode
     private String barcode = "";
-    //text of the decoding error
     private static final String DECODE_ERROR = "ERROR: Barcode decoding unsuccessful, please try again.";
-    //text of the possible exception related to the CountdownLatch
-    private static final String LATCH_ERROR = "Fatal error! Please try again.";
-    //possibility to limit barcode format recognized to improve performance
-    private FirebaseVisionBarcodeDetectorOptions options =
-            new FirebaseVisionBarcodeDetectorOptions.Builder()
-                    .setBarcodeFormats(
-                            FirebaseVisionBarcode.FORMAT_ALL_FORMATS).build();
-    //cause of different possible implementations, the detector instantiation is leaved detached
-    private final FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+    private final FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
+    private CountDownLatch latch = new CountDownLatch(1);
 
     /**
      * implementation of the decodeBarcode method, that would detect the barcode from the given image
@@ -41,48 +30,55 @@ public class MLKitBarcode implements Barcode{
      */
     @Override
     public String decodeBarcode(Bitmap bitmap) {
-        //using a countdown latch to manage processing time and concurrency
-        final CountDownLatch latch = new CountDownLatch(1);
-        //using firebase .fromBitmap method to get an analyzable image from a given bitmap
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
-        //Get the detector, modularized from the main method, so it's possible to make independent changes
-        //method to find and decode a barcode in the image
-        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
-                //adding a listener for the success result
-                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
-                    @Override
-                    //it gets a List of barcodes as parameter, which is the output of the barcode detector
-                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
-                        if (!(barcodes.isEmpty())) {
-                            //if not empty get the string of the barcode value
-                            barcode = barcodes.get(0).getRawValue();
-                        }else{
-                            //if no barcode is found, return empty string
-                            barcode= "";
-                        }
-                        //get the countdown to zero in order to proceed with the method
-                        latch.countDown();
-                    }
-                })
-                //then adding the listener for the case of failure
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        latch.countDown();
-                        //in case of failure return a string that report the error occurred
-                        barcode = DECODE_ERROR;
-                        Log.e("Error Barcode", e.getMessage());
-                    }
-                });
+
+        //get the FirebaseImage obj from the bitmap
+        final FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        detectTheBarcode(image);
+
+        //wait for the detection to be completed
         try {
-            //waiting for the latch to be 0
             latch.await();
-        }catch(InterruptedException e){
-            Log.e("Latch exception", e.getMessage());
-            return LATCH_ERROR;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
         return barcode;
     }
 
+    /**
+     * Take the image and detect the barcodes in it using Firebase
+     * @param image the image that will be scanned from Firebase
+     * @modify barcode is set with the string of the code retrieved
+     */
+    private void detectTheBarcode(FirebaseVisionImage image) {
+
+        //Firebase method to detect barcodes inside an image
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+
+                    //on success - set the barcode to the string retrieved from the image, empty string if no barcode is found
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                        if (!barcodes.isEmpty()) {
+                            barcode = barcodes.get(0).getRawValue();
+                        }else{
+                            barcode= "";
+                        }
+                        latch.countDown();
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+
+                    //on fail - return an error on decoding the barcode
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        barcode = DECODE_ERROR;
+                        latch.countDown();
+                    }
+                });
+    }
 
 }
